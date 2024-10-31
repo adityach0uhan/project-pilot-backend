dab9f3b39f38797aaa0b309d654cff23884d54a1
adityach0uhan <m4with6x@gmail.com>
2024-10-31 23:45:41 +0530

    createa a teacher data validator using express-validator

---
+++ src/validators/teacher.validator.ts
@@ -0,0 +1,24 @@
+import { body } from 'express-validator';
+
+export const teacherRegisterValidation = [
+    body('name').isString().notEmpty().withMessage('Name is required'),
+    body('profilePic')
+        .isString()
+        .optional()
+        .withMessage('Profile picture is optional'),
+    body('email').isEmail().notEmpty().withMessage('Email is required'),
+    body('department')
+        .isString()
+        .notEmpty()
+        .withMessage('Department is required'),
+    body('password').isString().notEmpty().withMessage('Password is required'),
+    body('designation')
+        .isString()
+        .notEmpty()
+        .withMessage('Designation is required'),
+    body('semester').isNumeric().notEmpty().withMessage('Semester is required'),
+    body('employeeId')
+        .isString()
+        .notEmpty()
+        .withMessage('Employee ID is required')
+];
