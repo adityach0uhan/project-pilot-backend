f0f9f447935fbf45c8c027fcfabc28e2c604d518
adityach0uhan <m4with6x@gmail.com>
2024-10-31 22:23:38 +0530

    Setup Cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 import connectDataBase from './database/db.config.js';
 import 'dotenv/config';
 
 const app: Application = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const corsOptions = {
+    origin: function (
+        origin: string | undefined,
+        callback: (err: Error | null, allow?: boolean) => void
+    ) {
+        const allowedOrigins = ['http://localhost:3000', 'http://example2.com'];
+        if (allowedOrigins.indexOf(origin || '') !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, world!');
