6a424176d336e75639e12b5ea5db945967012d09
adityach0uhan <m4with6x@gmail.com>
2024-10-31 21:57:42 +0530

    created teacher schema

---
+++ src/schema/teacher.schema.ts
@@ -0,0 +1,32 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface ITeacher extends Document {
+    name: string;
+    email: string;
+    profilePic: string;
+    department: string;
+    password: string;
+    role: string;
+    employeeId: string;
+    semester: number;
+}
+
+const teacherSchema = new Schema<ITeacher>(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        profilePic: { type: String, required: false },
+        password: { type: String, required: true },
+        department: { type: String, required: true },
+        role: { type: String, required: true, default: 'teacher' },
+        semester: { type: Number, required: true },
+        employeeId: { type: String, required: true, unique: true }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const TeacherModel = model<ITeacher>('Teacher', teacherSchema);
+
+export default TeacherModel;
