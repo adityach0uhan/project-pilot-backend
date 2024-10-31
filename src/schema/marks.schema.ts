a1d7224a91b5bed1c465e2497af8189f216a677b
adityach0uhan <m4with6x@gmail.com>
2024-10-31 21:57:53 +0530

    created marks schema

---
+++ src/schema/marks.schema.ts
@@ -0,0 +1,30 @@
+import { Schema, Document, model } from 'mongoose';
+
+export interface IMark extends Document {
+    project: Schema.Types.ObjectId;
+    score: number;
+    teacher: Schema.Types.ObjectId;
+}
+
+const markSchema = new Schema<IMark>(
+    {
+        project: {
+            type: Schema.Types.ObjectId,
+            ref: 'Project',
+            required: true
+        },
+        score: { type: Number, required: false, default: 0 },
+        teacher: {
+            type: Schema.Types.ObjectId,
+            ref: 'Teacher',
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const MarkModel = model<IMark>('Mark', markSchema);
+
+export default MarkModel;
