1e6d3be42b59498bb9991757c39c03a947e5a144
adityach0uhan <m4with6x@gmail.com>
2024-10-31 21:57:01 +0530

    created group schema

---
+++ src/schema/group.schema.ts
@@ -0,0 +1,50 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface IGroup extends Document {
+    name: string;
+    groupNumber: number;
+    members: Schema.Types.ObjectId[];
+    project: Schema.Types.ObjectId;
+    createdBy: Schema.Types.ObjectId;
+    joiningCode: string;
+    groupleader: Schema.Types.ObjectId;
+    createdAt?: Date;
+}
+
+const groupSchema = new Schema<IGroup>(
+    {
+        name: { type: String, required: true },
+        groupNumber: { type: Number, required: true },
+        members: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Student',
+                required: true
+            }
+        ],
+        project: {
+            type: Schema.Types.ObjectId,
+            ref: 'Project',
+            required: true
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'Student',
+            required: true
+        },
+        joiningCode: { type: String, required: true },
+        groupleader: {
+            type: Schema.Types.ObjectId,
+            ref: 'Student',
+            required: true
+        }
+    },
+
+    {
+        timestamps: true
+    }
+);
+
+const GroupModel = model<IGroup>('Group', groupSchema);
+
+export default GroupModel;
