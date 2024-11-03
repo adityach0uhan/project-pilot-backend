49f6e2b5abb1f3e3f0eab2f91b1c98bd6a3dd4f5
adityach0uhan <m4with6x@gmail.com>
2024-11-03 12:44:34 +0530

    added group leader check

--- src/controllers/group.controllers.ts
+++ src/controllers/group.controllers.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import GroupModel from '../schema/group.schema.js';
 import { generateUniqueInviteCodes } from '../utils/generateUniqueInviteCodes.js';
+import exp from 'constants';
 
 export const createNewGroup = async (
     req: Request,
     res: Response,
     next: NextFunction
-) => {
+): Promise<void> => {
     try {
         const {
             name,
@@ -17,6 +18,11 @@ export const createNewGroup = async (
             groupleader,
             semester
         } = req.body;
+
+        if (!name || !groupNumber || !createdBy || !groupleader) {
+            res.status(400).json({ message: 'Missing required fields' });
+            return;
+        }
 
         const newInviteCode = await generateUniqueInviteCodes();
 
@@ -34,10 +40,225 @@ export const createNewGroup = async (
         await newGroup.save();
 
         res.status(201).json({ message: 'Group created successfully' });
-    } catch (error) {
+    } catch (error: any) {
         res.status(500).json({
             message: 'Internal server error',
-            error: error
+            error: error.message
+        });
+    }
+};
+
+export const joinGroup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { inviteCode } = req.params;
+        const { userId } = req.body;
+
+        const group = await GroupModel.findOne({ inviteCode });
+
+        if (!group) {
+            res.status(404).json({ message: 'Group not found' });
+            return;
+        }
+
+        if (group.members.includes(userId)) {
+            res.status(400).json({
+                message: 'User is already a member of this group'
+            });
+            return;
+        }
+
+        group.members.push(userId);
+        await group.save();
+
+        res.status(200).json({ message: 'User joined group successfully' });
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message
+        });
+    }
+};
+
+export const getGroupInfo = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { userId } = req.body;
+
+        const group = await GroupModel.findOne({ members: userId })
+            .select(
+                'name groupNumber project inviteCode semester createdAt updatedAt'
+            )
+            .populate({
+                path: 'members',
+                select: 'email name'
+            })
+            .populate({
+                path: 'createdBy',
+                select: 'name'
+            })
+            .populate({
+                path: 'groupleader',
+                select: 'name'
+            });
+
+        if (!group) {
+            res.status(404).json({ message: 'Group not found' });
+            return;
+        }
+
+        res.status(200).json({ group });
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message
+        });
+    }
+};
+
+export const getAllGroups = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { semester } = req.body;
+        const groups = await GroupModel.find({ semester }).select(
+            'name groupNumber project inviteCode semester createdAt updatedAt'
+        );
+
+        if (!groups) {
+            res.status(404).json({ message: 'No groups found' });
+            return;
+        }
+
+        res.status(200).json({ groups });
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message
+        });
+    }
+};
+
+export const getGroup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { groupId } = req.params;
+
+        const group = await GroupModel.findById({ _id: groupId });
+
+        if (!group) {
+            res.status(404).json({ message: 'Group not found' });
+            return;
+        }
+
+        res.status(200).json({ group });
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message
+        });
+    }
+};
+
+export const updateGroup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { groupId } = req.params;
+        const { name, groupNumber, project, semester } = req.body;
+        //TODO: Add check if the user is the group leader or the creator of the group
+        const group = await GroupModel.findById(
+            { _id: groupId },
+            { new: true }
+        );
+
+        if (!group) {
+            res.status(404).json({ message: 'Group not found' });
+            return;
+        }
+
+        group.name = name || group.name;
+        group.groupNumber = groupNumber || group.groupNumber;
+        group.project = project || group.project;
+        group.semester = semester || group.semester;
+
+        await group.save();
+
+        res.status(200).json({ message: 'Group updated successfully', group });
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message
+        });
+    }
+};
+
+export const deleteGroup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { groupId } = req.params;
+        //TODO: Add check if the user is the group leader or the creator of the group
+        const group = await GroupModel.findByIdAndDelete({ _id: groupId });
+
+        if (!group) {
+            res.status(404).json({ message: 'Group not found' });
+            return;
+        }
+
+        res.status(200).json({ message: 'Group deleted successfully' });
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message
+        });
+    }
+};
+
+export const kickMember = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { groupId, memberId } = req.params;
+        //TODO: Add check if the user is the group leader or the creator of the group
+        const group = await GroupModel.findByIdAndUpdate(
+            groupId,
+            {
+                $pull: { members: memberId }
+            },
+            { new: true }
+        );
+
+        if (!group) {
+            res.status(404).json({ message: 'Group not found' });
+            return;
+        } else {
+            res.status(200).json({
+                message: 'Member kicked from group successfully',
+                group
+            });
+        }
+    } catch (error: any) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message
         });
     }
 };
