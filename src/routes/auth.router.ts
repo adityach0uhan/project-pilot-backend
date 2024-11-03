8a6a1806fd4f9fd1f92708385eff695ef1e9c779
adityach0uhan <m4with6x@gmail.com>
2024-11-03 12:45:33 +0530

    added auth router

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,18 +1,46 @@
-import express from 'express';
+import express, { Router } from 'express';
 import { studentRegisterValidation } from '../validators/student.validator.js';
 import { teacherRegisterValidation } from '../validators/teacher.validator.js';
 import {
     studentRegister,
     studentLogin,
+    studentForgetPassword,
+    studentChangePassword,
+    studentOtpGenerate
+} from '../controllers/auth.student.controllers.js';
+import {
     teacherRegister,
-    teacherLogin
-} from '../controllers/auth.controllers.js';
+    teacherLogin,
+    teacherOtpGenerate,
+    teacherForgetPassword,
+    teacherChangePassword
+} from '../controllers/auth.teacher.controllers.js';
+import { checkLogin } from '../middlewares/checkLogin.js';
+import { checkTeacher } from '../middlewares/checkTeacher.js';
+const router: Router = express.Router();
 
-const router = express.Router();
-
-router.post('/student/login', studentLogin);
-router.post('/teacher/login', teacherLogin);
+//Registration routes (Test Done)
 router.post('/student/register', studentRegisterValidation, studentRegister);
 router.post('/teacher/register', teacherRegisterValidation, teacherRegister);
+
+//Login routes (Test Done)
+router.post('/student/login', studentLogin);
+router.post('/teacher/login', teacherLogin);
+
+//reset password
+router.put('/student/change-password', checkLogin, studentChangePassword);
+router.put(
+    '/teacher/change-password',
+    checkLogin,
+    checkTeacher,
+    teacherChangePassword
+);
+
+// forgot password (With Email based OTP) (OTP Genetaion Done)(Test Done)
+router.post('/student/forgot-password-otp-generate', studentOtpGenerate);
+router.post('/teacher/forgot-password-otp-generate', teacherOtpGenerate);
+//(Test Done)
+router.post('/student/forgot-password', studentForgetPassword);
+router.post('/teacher/forgot-password', teacherForgetPassword);
 
 export default router;
