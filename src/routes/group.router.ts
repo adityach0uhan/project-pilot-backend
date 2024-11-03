0802dbe2f0d53a5e26dd7d90d53e66f3e30e388f
adityach0uhan <m4with6x@gmail.com>
2024-11-03 12:44:12 +0530

    completed group router

--- src/routes/group.router.ts
+++ src/routes/group.router.ts
@@ -1,13 +1,26 @@
 import { Router } from 'express';
-import { createNewGroup } from '../controllers/group.controllers.js';
+import {
+    createNewGroup,
+    joinGroup,
+    getGroupInfo,
+    getAllGroups,
+    getGroup,
+    updateGroup,
+    deleteGroup,
+    kickMember
+} from '../controllers/group.controllers.js';
+import { checkLogin } from '../middlewares/checkLogin.js';
+import { checkTeacher } from '../middlewares/checkTeacher.js';
 
 const router = Router();
 
 router.post('/create', createNewGroup);
-// router.post('/join', joinGroup);
-// router.get('/all', getAllGroups);
-// router.get('/:groupId', getGroup);
-// router.put('/:groupId', updateGroup);
-// router.delete('/:groupId', deleteGroup);
+router.post('/join/:inviteCode', joinGroup);
+router.put('/:groupId/kick/:memberId', kickMember);
+router.get('/groupInfo', checkLogin, getGroupInfo);
+router.get('/getAllGroups', getAllGroups);
+router.get('/:groupId', getGroup);
+router.put('/:groupId', updateGroup);
+router.delete('/:groupId', deleteGroup);
 
 export default router;
