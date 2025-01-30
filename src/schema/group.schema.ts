f1561e4e89a620b2b5f9e4a78d75561e6eea8a44
adityach0uhan <m4with6x@gmail.com>
2025-01-30 23:16:01 +0530

    group schema changes

--- src/schema/group.schema.ts
+++ src/schema/group.schema.ts
@@ -1,57 +1,60 @@
 import { Schema, model, Document } from 'mongoose';
+import { IStudent } from './student.schema.js'; // reference to student
+import { IProject } from './project.schema.js'; // reference to project
 
 export interface IGroup extends Document {
     name: string;
-    groupNumber: number;
+    groupNumber: string;
+    collegeId: Schema.Types.ObjectId;
     members: Schema.Types.ObjectId[];
-    project: Schema.Types.ObjectId;
-    createdBy: Schema.Types.ObjectId;
-    inviteCode: string;
     groupleader: Schema.Types.ObjectId;
-    semester: number;
-    pendingRequests?: Schema.Types.ObjectId[];
+    semester: string;
+    projectId: Schema.Types.ObjectId;
+    projectName: string;
+    createdBy?: IStudent;
+    pendingRequests?: IStudent[];
+    inviteCode?: string;
 }
 
 const groupSchema = new Schema<IGroup>(
     {
         name: { type: String, required: true },
-        groupNumber: { type: Number, required: true },
+        groupNumber: { type: String, required: true },
         members: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Student',
-                required: true
-            }
+            { type: Schema.Types.ObjectId, ref: 'Student', required: true }
         ],
-        project: {
-            type: Schema.Types.ObjectId,
-            ref: 'Project',
-            required: false,
-            default: null
-        },
-        createdBy: {
-            type: Schema.Types.ObjectId,
-            ref: 'Student',
-            required: true
-        },
-        inviteCode: { type: String, required: true },
-        semester: { type: Number, required: true },
         groupleader: {
             type: Schema.Types.ObjectId,
             ref: 'Student',
             required: true
         },
+        projectId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Project',
+            required: true
+        },
+        projectName: { type: String, required: true },
+        semester: { type: String, required: true },
+        collegeId: {
+            type: Schema.Types.ObjectId,
+            ref: 'College',
+            required: true
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'Student',
+            required: false
+        },
         pendingRequests: [
             {
                 type: Schema.Types.ObjectId,
-                ref: 'Student'
+                ref: 'Student',
+                required: false
             }
-        ]
+        ],
+        inviteCode: { type: String, required: false }
     },
-
-    {
-        timestamps: true
-    }
+    { timestamps: true }
 );
 
 const GroupModel = model<IGroup>('Group', groupSchema);
