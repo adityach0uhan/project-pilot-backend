9272557ac79b197839c3dca9698b5819015bf651
adityach0uhan <m4with6x@gmail.com>
2025-02-01 23:51:29 +0530

    changes in colleg router

---
+++ src/routes/college.router.ts
@@ -0,0 +1,25 @@
+import exp from 'constants';
+import { Router } from 'express';
+import {
+    deleteStudentById,
+    getAllStudents,
+    getAllTeachers,
+    getStudentById,
+    getTeacherById,
+    deleteTeacherById
+} from '../controllers/auth.college.controller.js';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello from College Router');
+});
+
+router.get('/allstudents/:collegeId', getAllStudents);
+router.get('/allteachers/:collegeId', getAllTeachers);
+router.get('/student/:id', getStudentById);
+router.get('/teacher/:id', getTeacherById);
+router.delete('/student/:studentId', deleteStudentById);
+router.delete('/teacher/:teacherId', deleteTeacherById);
+
+export default router;
